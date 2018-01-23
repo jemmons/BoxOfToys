3efdc696fb3c66d9dcc1fb306aa18a56e1415049
Joshua Emmons <skia@skia.net>
2018-01-23 14:01:35 -0600

    Get hex values out of UIColor.

--- Sources/Extensions/UIColor+Hex.swift
+++ Sources/Extensions/UIColor+Hex.swift
@@ -9,4 +9,20 @@ public extension UIColor {
     let blue = CGFloat(hex & 0x0000FF) / 255.0
     self.init(red: red, green: green, blue: blue, alpha: 1.0)
   }
+  
+  
+  var hex: UInt? {
+    var red : CGFloat = 0
+    var green : CGFloat = 0
+    var blue : CGFloat = 0
+    guard getRed(&red, green: &green, blue: &blue, alpha: nil) else {
+      return nil
+    }
+    
+    let redComp = UInt(red * 255) << 16
+    let greenComp = UInt(green * 255) << 8
+    let blueComp = UInt(blue * 255)
+    
+    return redComp + greenComp + blueComp
+  }
 }
--- Tests/UIColor+HexTests.swift
+++ Tests/UIColor+HexTests.swift
@@ -4,19 +4,41 @@ import BoxOfToys
 
 
 class UIColorHexTests: XCTestCase {
-  func testRed() {
+  func testFromHexRed() {
     XCTAssertEqual(UIColor(hex: 0xFF0000), UIColor(red: 1, green: 0, blue: 0, alpha: 1))
   }
 
-  func testGreen() {
+  func testFromHexGreen() {
     XCTAssertEqual(UIColor(hex: 0x00FF00), UIColor(red: 0, green: 1, blue: 0, alpha: 1))
   }
   
-  func testBlue() {
+  func testFromHexBlue() {
     XCTAssertEqual(UIColor(hex: 0x0000FF), UIColor(red: 0, green: 0, blue: 1, alpha: 1))
   }
   
-  func testMinuscule() {
+  func testMinusculeHex() {
     XCTAssertEqual(UIColor(hex: 0xffaa00), UIColor(red: 1, green: 170.0/255.0, blue: 0, alpha: 1))
+  }
+  
+  
+  func testToHexRed() {
+    XCTAssertEqual(UIColor(red: 170/255, green: 0, blue: 0, alpha: 1).hex, 0xaa0000)
+  }
+  
+
+  func testToHexGreen() {
+    XCTAssertEqual(UIColor(red: 0, green: 170/255, blue: 0, alpha: 1).hex, 0x00aa00)
+  }
+
+
+  func testToHexBlue() {
+    XCTAssertEqual(UIColor(red: 0, green: 0, blue: 170/255, alpha: 1).hex, 0x0000aa)
+  }
+
+
+  func testRoundTrip() {
+    XCTAssertEqual(UIColor(hex: 0xabc123).hex, 0xabc123)
+    XCTAssertEqual(UIColor(hex: 0x0dead0).hex, 0x0dead0)
+    XCTAssertEqual(UIColor(hex: 0xbadf00).hex, 0xbadf00)
   }
 }
