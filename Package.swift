c03eaffe7c02f7adca71f85b925705f5901f1573
Joshua Emmons <skia@skia.net>
2019-07-12 14:15:30 -0500

    Testing SPM for Xcode 11

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BoxOfToys",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "BoxOfToys",
+            targets: ["BoxOfToys"]),
+    ],
+    dependencies: [
+      .package(url: "https://github.com/jemmons/BagOfTricks.git", from: "1.0.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "BoxOfToys",
+            dependencies: ["BagOfTricks"]),
+        .testTarget(
+            name: "BoxOfToysTests",
+            dependencies: ["BoxOfToys"]),
+    ]
+)
